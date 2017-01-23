30853cb19a44fe6993426811e8b39eb05f22cebc
KarolinaNik <karolinanik@gmail.com>
2017-01-23 10:30:49 +0000

    Added Inches class

---
+++ src/main/java/gl2_CharArithDemo.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Alexandra on 23/01/2017.
+ */
+public class gl2_CharArithDemo {
+
+    public static void main(String[] args) {
+        char ch = 'X';
+
+        System.out.println("ch содержит значение " + ch);
+
+        ch++; //инкрементировать переменную ch
+        System.out.println("теперь ch содержит " + ch);
+
+        ch = 90;
+        System.out.println("теперь ch содержит " + ch);
+
+
+    }
+}
---
+++ src/main/java/gl2_Hypot.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Alexandra on 23/01/2017.
+ */
+public class gl2_Hypot {
+    public static void main(String[] args) {
+        double x, y, z;
+
+        x = 3;
+        y = 4;
+
+        z = Math.sqrt(x * x + y * y);
+        System.out.println("Длина гипотенузы при катетах x = " + x + " и y = " + y + " равна " + z);
+
+    }
+}
